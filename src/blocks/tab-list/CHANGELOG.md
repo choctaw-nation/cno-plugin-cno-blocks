a32f881135f7d82cb8dcb99e6d6d99a4d284df7c
KJ Roelke <kroelke@choctawnation.com>
2026-06-22 14:06:26 -0500

    add new block control

---
+++ src/blocks/tab-list/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## v1.1.0
+
+-   Added: Control to allow tabs to scroll when too many for mobile devices
--- src/blocks/tab-list/Edit.tsx
+++ src/blocks/tab-list/Edit.tsx
@@ -4,9 +4,12 @@
 import {
 	useBlockProps,
 	useInnerBlocksProps,
+	InspectorControls,
 	store as blockEditorStore,
 } from '@wordpress/block-editor';
+import { PanelBody, ToggleControl } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
+import { useState, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -17,14 +20,29 @@ import './editor.scss';
 
 const TAB_LIST_TEMPLATE = [ [ 'cno/tab' ], [ 'cno/tab' ] ];
 
-function Edit( { clientId } ) {
+function Edit( { clientId, attributes, setAttributes } ) {
+	const { allowOverflow } = attributes;
+	const [ shouldOverflow, setShouldOverflow ] = useState( allowOverflow );
 	const tabsClientId = useSelect(
 		( select ) =>
 			select( blockEditorStore ).getBlockRootClientId( clientId ),
 		[ clientId ]
 	);
 
-	const blockProps = useBlockProps();
+	const flexWrapEnabled = attributes?.layout?.flexWrap !== 'nowrap';
+	useEffect( () => {
+		// If flex wrap is enabled, overflow should be disabled and the attribute updated accordingly.
+		if ( flexWrapEnabled ) {
+			setShouldOverflow( false );
+			setAttributes( { allowOverflow: false } );
+		} else {
+			setAttributes( { allowOverflow: shouldOverflow } );
+		}
+	}, [ flexWrapEnabled, allowOverflow, setAttributes, shouldOverflow ] );
+
+	const blockProps = useBlockProps( {
+		className: allowOverflow ? 'allow-overflow' : undefined,
+	} );
 
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
 		allowedBlocks: [ 'cno/tab' ],
@@ -36,6 +54,24 @@ function Edit( { clientId } ) {
 
 	return (
 		<>
+			<InspectorControls>
+				<PanelBody title="Tab List Settings">
+					<ToggleControl
+						__nextHasNoMarginBottom
+						label="Allow Overflow"
+						checked={ shouldOverflow }
+						onChange={ ( value ) => {
+							setShouldOverflow( value );
+						} }
+						disabled={ flexWrapEnabled }
+						help={
+							flexWrapEnabled
+								? 'Overflow is disabled when flex wrap is enabled.'
+								: 'Allow tabs to overflow (horizontally) when there are too many to fit in the available space.'
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
 			<AddTabToolbarControl tabsClientId={ tabsClientId } />
 			<RemoveTabToolbarControl tabsClientId={ tabsClientId } />
 			<div { ...innerBlocksProps } />
